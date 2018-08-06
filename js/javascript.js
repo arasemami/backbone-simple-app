9e459394847d085abd9294eae67350d60d4a9d4b
aras armani <aras.armani2014@gmail.com>
2018-08-06 08:22:01 +0300

    backbone start

---
+++ js/javascript.js
@@ -0,0 +1,88 @@
+// Backbone Model
+
+var Blog = Backbone.Model.extend({
+    defaults:{
+        author:'',
+        title:'',
+        url:''
+
+    },
+
+
+});
+
+
+// Backbone Collaction
+
+var Blogs = Backbone.Collection.extend({});
+
+
+
+// Instantiate two blos
+
+var blog1 = new Blog({
+    author:'aras',
+    title:'aras hast khali khob',
+    url:'www.aras.com'
+
+});
+
+var blog2 = new Blog({
+    author:'omid',
+    title:'omid hast khali khob',
+    url:'www.omid.com'
+
+});
+
+
+// Instantiate Collection
+
+var blogs = new Blogs([blog1,blog2]);
+
+
+
+// Backbone Views
+
+
+var BlogView = Backbone.View.extend({
+
+    model: new Blog(),
+    tagName:'tr',
+    initialize: function(){
+        this.template  = _.template($('.blogs-list-template').html());
+    },
+    render: function(){
+        this.$el.html(this.template({model: this.model.toJSON()}))
+    }
+});
+
+// Backbone View All Blogs
+
+ var BlogsViews = Backbone.View.extend({
+
+     model: blogs,
+     el: $('.blogs-list'),
+     initialize: function() {
+         this.model.on('add',this.render(), this );
+     },
+     render: function(){
+         var self = this;
+        this.$el.html('');
+        _.each(this.model.toArray(), function(blog){
+            self.$el.append((new BlogView({model: blog})).render().$el);
+        });
+     }
+
+ });
+
+ $(document).ready(function(){
+    $('.add-blog').on('click', function(){
+        var blog = new Blog({
+            author: $('.author-input').val(),
+            title: $('.title-input').val(),
+            url: $('.url-input').val()
+
+        });
+        console.log(blog);
+    })
+ });
